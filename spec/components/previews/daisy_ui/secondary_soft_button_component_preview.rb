63de9ed6504fd963f4a2782a4407decad88979d4
greendrop <greendrop.devel@gmail.com>
2025-06-30 00:23:59 +0000

    fix: 不要なコメントを削除

--- spec/components/previews/daisy_ui/secondary_soft_button_component_preview.rb
+++ spec/components/previews/daisy_ui/secondary_soft_button_component_preview.rb
@@ -7,7 +7,7 @@ module DaisyUi
     SVG_ICON = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="size-[1.2em]"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>'
 
     layout 'component_preview'
-    # ...existing code...
+
     # @param content text
     # @param override_classes text
     # @param append_classes text
@@ -58,7 +58,6 @@ module DaisyUi
       ).with_content(content || '')
     end
 
-    # ...existing code...
     # @!group Behaviors
     def behavior_active(content: 'Active')
       render COMPONENT_CLASS.new(
@@ -72,7 +71,6 @@ module DaisyUi
       ).with_content(content)
     end
 
-    # ...existing code...
     # @!group Sizes
     def size_extra_small(content: 'Extra Small')
       render COMPONENT_CLASS.new(
@@ -104,7 +102,6 @@ module DaisyUi
       ).with_content(content)
     end
 
-    # ...existing code...
     # @!group Modifiers
     def modifier_wide(content: 'Wide')
       render COMPONENT_CLASS.new(
@@ -133,7 +130,6 @@ module DaisyUi
         circle: true
       ).with_content(content)
     end
-    # ...existing code...
 
     private
 
