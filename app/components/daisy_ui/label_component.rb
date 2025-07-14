66b71a877b2df696e3928bacb2991c53e498519b
greendrop <greendrop.devel@gmail.com>
2025-07-14 08:56:42 +0000

    feat: LabelComponentを追加し、スナップショットテストを作成

---
+++ app/components/daisy_ui/label_component.rb
@@ -0,0 +1,141 @@
+# frozen_string_literal: true
+
+module DaisyUi
+  class LabelComponent < ViewComponent::Base
+    DEFAULT_CLASSES = %w[label].freeze
+
+    erb_template <<~ERB
+      <%= tag.public_send(tag_name, **args) do %>
+        <%= content %>
+      <% end %>
+    ERB
+
+    def initialize(
+      tag_name: 'label',
+      override_classes: nil,
+      append_classes: nil,
+      data: nil,
+      disabled: nil,
+      tabindex: nil,
+      accesskey: nil,
+      id: nil,
+      title: nil,
+      style: nil,
+      dir: nil,
+      lang: nil,
+      onclick: nil,
+      ondblclick: nil,
+      onmousedown: nil,
+      onmouseup: nil,
+      onmouseover: nil,
+      onmousemove: nil,
+      onmouseout: nil,
+      onkeypress: nil,
+      onkeydown: nil,
+      onkeyup: nil
+    )
+      @override_classes = override_classes
+      @append_classes = append_classes
+      @tag_name = tag_name
+      @data = data
+      @disabled = disabled
+      @tabindex = tabindex
+      @accesskey = accesskey
+      @id = id
+      @title = title
+      @style = style
+      @dir = dir
+      @lang = lang
+      @onclick = onclick
+      @ondblclick = ondblclick
+      @onmousedown = onmousedown
+      @onmouseup = onmouseup
+      @onmouseover = onmouseover
+      @onmousemove = onmousemove
+      @onmouseout = onmouseout
+      @onkeypress = onkeypress
+      @onkeydown = onkeydown
+      @onkeyup = onkeyup
+
+      super
+    end
+
+    private
+
+    attr_reader :override_classes,
+                :append_classes,
+                :tag_name,
+                :data,
+                :disabled,
+                :tabindex,
+                :accesskey,
+                :id,
+                :title,
+                :style,
+                :dir,
+                :lang,
+                :onclick,
+                :ondblclick,
+                :onmousedown,
+                :onmouseup,
+                :onmouseover,
+                :onmousemove,
+                :onmouseout,
+                :onkeypress,
+                :onkeydown,
+                :onkeyup
+
+    def args
+      return @_args if @_args
+
+      values = {}
+      values.merge!(options) if options.present?
+
+      @_args = values
+    end
+
+    def options
+      values = {
+        data:,
+        disabled:
+      }.compact
+
+      values.merge!(html_options) if html_options.present?
+      values
+    end
+
+    def html_options
+      {
+        tabindex:,
+        accesskey:,
+        id:,
+        class: label_class,
+        title:,
+        style:,
+        dir:,
+        lang:,
+        onclick:,
+        ondblclick:,
+        onmousedown:,
+        onmouseup:,
+        onmouseover:,
+        onmousemove:,
+        onmouseout:,
+        onkeypress:,
+        onkeydown:,
+        onkeyup:
+      }.compact
+    end
+
+    def default_classes
+      self.class::DEFAULT_CLASSES.dup
+    end
+
+    def label_class
+      classes = default_classes
+      classes = override_classes if override_classes
+      classes << append_classes if append_classes
+      classes.join(' ')
+    end
+  end
+end
---
+++ spec/components/daisy_ui/label_component_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DaisyUi::LabelComponent, type: :component do
+  describe 'snapshot' do
+    let!(:snapshot_prefix) { described_class.name.underscore }
+    let!(:rendered) { render_inline(instance) }
+
+    shared_examples 'match snapshot' do
+      it { expect(rendered).to match_snapshot(snapshot_name) }
+    end
+
+    context 'default' do
+      let!(:instance) { described_class.new.with_content('Default Label') }
+      let!(:snapshot_name) { File.join(snapshot_prefix, 'default') }
+
+      it_behaves_like 'match snapshot'
+    end
+
+    context 'tag_nameを指定' do
+      let!(:instance) { described_class.new(tag_name: 'span').with_content('Span Label') }
+      let!(:snapshot_name) { File.join(snapshot_prefix, 'tag_name') }
+
+      it_behaves_like 'match snapshot'
+    end
+  end
+end
