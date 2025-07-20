a1917767f4733dcb205da47de1ef207716887a4a
greendrop <greendrop.devel@gmail.com>
2025-07-20 08:01:48 +0000

    refactor: SelectComponentPreviewの引数を整理し、冗長なオプションを削除

--- spec/components/previews/daisy_ui/select_component_preview.rb
+++ spec/components/previews/daisy_ui/select_component_preview.rb
@@ -23,28 +23,6 @@ module DaisyUi
     # @param disabled toggle
     # @param include_blank toggle
     # @param prompt text
-    # @param accept text
-    # @param readonly toggle
-    # @param tabindex text
-    # @param accesskey text
-    # @param id text
-    # @param title text
-    # @param style text
-    # @param dir text
-    # @param lang text
-    # @param onclick text
-    # @param ondblclick text
-    # @param onmousedown text
-    # @param onmouseup text
-    # @param onmouseover text
-    # @param onmousemove text
-    # @param onmouseout text
-    # @param onkeypress text
-    # @param onkeydown text
-    # @param onkeyup text
-    # @param onfocus text
-    # @param onblur text
-    # @param onchange text
     def default(
       name: 'color',
       option_tag: options_for_select([%w[Red red], %w[Green green], %w[Blue blue]]),
@@ -59,29 +37,7 @@ module DaisyUi
       multiple: nil,
       disabled: nil,
       include_blank: nil,
-      prompt: nil,
-      accept: nil,
-      readonly: nil,
-      tabindex: nil,
-      accesskey: nil,
-      id: nil,
-      title: nil,
-      style: nil,
-      dir: nil,
-      lang: nil,
-      onclick: nil,
-      ondblclick: nil,
-      onmousedown: nil,
-      onmouseup: nil,
-      onmouseover: nil,
-      onmousemove: nil,
-      onmouseout: nil,
-      onkeypress: nil,
-      onkeydown: nil,
-      onkeyup: nil,
-      onfocus: nil,
-      onblur: nil,
-      onchange: nil
+      prompt: nil
     )
       render COMPONENT_CLASS.new(
         name:,
@@ -97,29 +53,7 @@ module DaisyUi
         multiple:,
         disabled:,
         include_blank:,
-        prompt:,
-        accept:,
-        readonly:,
-        tabindex:,
-        accesskey:,
-        id:,
-        title:,
-        style:,
-        dir:,
-        lang:,
-        onclick:,
-        ondblclick:,
-        onmousedown:,
-        onmouseup:,
-        onmouseover:,
-        onmousemove:,
-        onmouseout:,
-        onkeypress:,
-        onkeydown:,
-        onkeyup:,
-        onfocus:,
-        onblur:,
-        onchange:
+        prompt:
       )
     end
 
