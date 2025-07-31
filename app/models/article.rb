4825858dd757a0620c2317cd41ddaa96d0e2a075
greendrop <greendrop.devel@gmail.com>
2025-07-31 20:29:09 +0000

    feat: annotaterbを追加し、モデルにスキーマ情報を注釈として追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: articles
+#
+#  id                  :bigint           not null, primary key
+#  body                :text
+#  published_at        :datetime
+#  status              :integer
+#  title               :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  article_category_id :bigint           not null
+#
+# Indexes
+#
+#  index_articles_on_article_category_id  (article_category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (article_category_id => article_categories.id)
+#
 class Article < ApplicationRecord
   belongs_to :article_category
 end
--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: article_categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class ArticleCategory < ApplicationRecord
 end
