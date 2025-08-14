683d77db61ad0a5d8104d35632601087166bd612
greendrop <greendrop.devel@gmail.com>
2025-08-14 08:01:20 +0000

    feat: WarningAlertコンポーネントとそのプレビューおよびスナップショットを追加

---
+++ app/components/daisy_ui/warning_alert_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module DaisyUi
+  class WarningAlertComponent < AlertComponent
+    DEFAULT_CLASSES = ['alert', 'alert-warning', 'alert-vertical', 'sm:alert-horizontal'].freeze
+  end
+end
