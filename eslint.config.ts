9dfdc7fcf0f3a35cd9b97c446e52f3e0ed6691fb
greendrop <greendrop.devel@gmail.com>
2025-08-17 08:00:59 +0000

    feat: ESLintとPrettierの設定ファイルを追加し、package.jsonにスクリプトを追加

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+// export default defineConfig([
+export default tseslint.config([
+  {
+    files: [
+      "**/*.{js,mjs,cjs,ts,mts,cts}"
+    ],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier
+]);
