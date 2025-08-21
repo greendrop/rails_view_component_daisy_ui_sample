e9cfe2d723310808d622a5dbb1897c0ee3f8b31d
greendrop <greendrop.devel@gmail.com>
2025-08-21 03:40:54 +0000

    feat: AdminSiteおよびUserSiteのレイアウトを追加し、アプリケーションの構造を整備

--- app/controllers/user_site/application_controller.rb
+++ app/controllers/user_site/application_controller.rb
@@ -4,5 +4,7 @@ module UserSite
   class ApplicationController < ActionController::Base
     # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
     allow_browser versions: :modern
+
+    layout 'user_site/layouts/application'
   end
 end
