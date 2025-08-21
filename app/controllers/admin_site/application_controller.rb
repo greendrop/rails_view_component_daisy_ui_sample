ad22fcd80f7282279b033e3683823cc39b0e3324
greendrop <greendrop.devel@gmail.com>
2025-08-21 03:32:21 +0000

    feat: AdminSiteおよびUserSiteのコントローラーとビューを追加し、ルーティングを設定

---
+++ app/controllers/admin_site/application_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module AdminSite
+  class ApplicationController < ActionController::Base
+    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+    allow_browser versions: :modern
+  end
+end
