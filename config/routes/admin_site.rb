1e1470ff138e6b9167359088067d3f2c3a2a2e2c
greendrop <greendrop.devel@gmail.com>
2025-08-23 22:56:50 +0000

    feat: admin_siteおよびuser_siteのスコープに名前を追加

--- config/routes/admin_site.rb
+++ config/routes/admin_site.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-scope module: :admin_site, path: '/admin' do
+scope module: :admin_site, path: '/admin', as: :admin_site do
   resource :homes, only: [:show]
 
   resources :articles
