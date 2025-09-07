db515668e7a64ee3014e9ee1c8c2a35a2b7bf6e0
greendrop <greendrop.devel@gmail.com>
2025-09-07 07:15:43 +0000

    feat: DaisyUi::PaginationComponentとDaisyUi::PaginationLinkButtonItemComponentを追加し、スナップショットテストを実装

---
+++ spec/components/daisy_ui/pagination_component_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DaisyUi::PaginationComponent, type: :component do
+  describe 'snapshot' do
+    include ActionView::Helpers::OutputSafetyHelper
+
+    let!(:snapshot_prefix) { described_class.name.underscore }
+    let!(:rendered) { render_inline(instance) }
+
+    shared_examples 'match snapshot' do
+      it { expect(rendered).to match_snapshot(snapshot_name) }
+    end
+
+    context 'default' do
+      let!(:instance) do
+        described_class.new.with_content(
+          safe_join(
+            [
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#', disabled: true).with_content('« First')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#', disabled: true).with_content('‹ Prev')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#', active: true).with_content('1')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#').with_content('2')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#').with_content('3')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#', disabled: true).with_content('…')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#').with_content('11')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#').with_content('12')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#').with_content('13')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#').with_content('Next ›')
+              ),
+              render_component_outside_view_context(
+                DaisyUi::PaginationLinkButtonItemComponent.new(url: '#').with_content('Last »')
+              )
+            ]
+          )
+        )
+      end
+      let!(:snapshot_name) { File.join(snapshot_prefix, 'default') }
+
+      it_behaves_like 'match snapshot'
+    end
+  end
+end
