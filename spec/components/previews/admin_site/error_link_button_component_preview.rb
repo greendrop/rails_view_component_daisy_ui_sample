fceb3a9bdbf75caccf3c1e794207fd1f24720364
greendrop <greendrop.devel@gmail.com>
2025-09-11 20:34:16 +0000

    feat: ErrorLinkButtonComponentPreviewにurlおよびidパラメータを追加

--- spec/components/previews/admin_site/error_link_button_component_preview.rb
+++ spec/components/previews/admin_site/error_link_button_component_preview.rb
@@ -9,10 +9,11 @@ module AdminSite
     layout 'daisy_ui/layouts/component_preview'
 
     # @label Default
-    # @param url text
     # @param content text
+    # @param url text
     # @param override_classes text
     # @param append_classes text
+    # @param id text
     # @param size select :size_options
     # @param responsive_extra_small_size select :size_options
     # @param responsive_small_size select :size_options
@@ -30,6 +31,7 @@ module AdminSite
       url: '#',
       override_classes: nil,
       append_classes: nil,
+      id: nil,
       size: nil,
       responsive_extra_small_size: nil,
       responsive_small_size: nil,
@@ -44,8 +46,10 @@ module AdminSite
       disabled: nil
     )
       render COMPONENT_CLASS.new(
+        url:,
         override_classes: override_classes ? [override_classes] : nil,
         append_classes: append_classes ? [append_classes] : nil,
+        id:,
         size:,
         responsive_extra_small_size:,
         responsive_small_size:,
@@ -57,27 +61,20 @@ module AdminSite
         square:,
         circle:,
         active:,
-        disabled:,
-        url:
+        disabled:
       ).with_content(content || '')
     end
 
     # @!group Behaviors
 
     # @label Active
-    def behavior_active(content: 'Active', url: '#')
-      render COMPONENT_CLASS.new(
-        active: true,
-        url:
-      ).with_content(content)
+    def behavior_active
+      default(active: true, content: 'Active', url: '#')
     end
 
     # @label Disabled
-    def behavior_disabled(content: 'Disabled', url: '#')
-      render COMPONENT_CLASS.new(
-        disabled: true,
-        url:
-      ).with_content(content)
+    def behavior_disabled
+      default(disabled: true, content: 'Disabled', url: '#')
     end
 
     # @!endgroup
@@ -85,43 +82,28 @@ module AdminSite
     # @!group Sizes
 
     # @label Extra Small
-    def size_extra_small(content: 'Extra Small', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_EXTRA_SMALL,
-        url:
-      ).with_content(content)
+    def size_extra_small
+      default(size: COMPONENT_CLASS::SIZE_EXTRA_SMALL, content: 'Extra Small', url: '#')
     end
 
     # @label Small
-    def size_small(content: 'Small', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_SMALL,
-        url:
-      ).with_content(content)
+    def size_small
+      default(size: COMPONENT_CLASS::SIZE_SMALL, content: 'Small', url: '#')
     end
 
     # @label Medium
-    def size_medium(content: 'Medium', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_MEDIUM,
-        url:
-      ).with_content(content)
+    def size_medium
+      default(size: COMPONENT_CLASS::SIZE_MEDIUM, content: 'Medium', url: '#')
     end
 
     # @label Large
-    def size_large(content: 'Large', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_LARGE,
-        url:
-      ).with_content(content)
+    def size_large
+      default(size: COMPONENT_CLASS::SIZE_LARGE, content: 'Large', url: '#')
     end
 
     # @label Extra Large
-    def size_extra_large(content: 'Extra Large', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_EXTRA_LARGE,
-        url:
-      ).with_content(content)
+    def size_extra_large
+      default(size: COMPONENT_CLASS::SIZE_EXTRA_LARGE, content: 'Extra Large', url: '#')
     end
 
     # @!endgroup
@@ -129,41 +111,23 @@ module AdminSite
     # @!group Modifiers
 
     # @label Wide
-    def modifier_wide(content: 'Wide', url: '#')
-      render COMPONENT_CLASS.new(
-        wide: true,
-        url:
-      ).with_content(content)
+    def modifier_wide
+      default(wide: true, content: 'Wide', url: '#')
     end
 
     # @label Block
-    def modifier_block(content: 'Block', url: '#')
-      render COMPONENT_CLASS.new(
-        block: true,
-        url:
-      ).with_content(content)
+    def modifier_block
+      default(block: true, content: 'Block', url: '#')
     end
 
     # @label Square
-    def modifier_square(
-      content: SVG_ICON.html_safe, # rubocop:disable Rails/OutputSafety
-      url: '#'
-    )
-      render COMPONENT_CLASS.new(
-        square: true,
-        url:
-      ).with_content(content)
+    def modifier_square
+      default(square: true, content: SVG_ICON.html_safe, url: '#') # rubocop:disable Rails/OutputSafety
     end
 
     # @label Circle
-    def modifier_circle(
-      content: SVG_ICON.html_safe, # rubocop:disable Rails/OutputSafety
-      url: '#'
-    )
-      render COMPONENT_CLASS.new(
-        circle: true,
-        url:
-      ).with_content(content)
+    def modifier_circle
+      default(circle: true, content: SVG_ICON.html_safe, url: '#') # rubocop:disable Rails/OutputSafety
     end
 
     # @!endgroup
