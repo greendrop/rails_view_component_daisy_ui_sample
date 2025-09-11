135b6b57007317dfa10c08205ed7eeb5b944656c
greendrop <greendrop.devel@gmail.com>
2025-09-11 19:32:31 +0000

    feat: LinkButtonComponentPreviewにurlおよびidパラメータを追加し、デフォルトメソッドを簡素化

--- spec/components/previews/admin_site/link_button_component_preview.rb
+++ spec/components/previews/admin_site/link_button_component_preview.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
+# frozen_string_literal: true
+
 module AdminSite
   # @logical_path components/admin_site/link_button
   class LinkButtonComponentPreview < Lookbook::Preview
     COMPONENT_CLASS = AdminSite::LinkButtonComponent
+    SVG_ICON = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="size-[1.2em]"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>' # rubocop:disable Layout/LineLength
 
-    include ViewComponent::TestExtHelpers
-
-    layout 'admin_site/layouts/component_preview'
+    layout 'daisy_ui/layouts/component_preview'
 
     # @label Default
     # @param content text
+    # @param url text
     # @param override_classes text
     # @param append_classes text
+    # @param id text
     # @param size select :size_options
     # @param responsive_extra_small_size select :size_options
     # @param responsive_small_size select :size_options
@@ -25,12 +28,12 @@ module AdminSite
     # @param circle toggle
     # @param active toggle
     # @param disabled toggle
-    # @param url text
     def default(
       content: 'Content',
       url: '#',
       override_classes: nil,
       append_classes: nil,
+      id: nil,
       size: nil,
       responsive_extra_small_size: nil,
       responsive_small_size: nil,
@@ -48,6 +51,7 @@ module AdminSite
         url:,
         override_classes: override_classes ? [override_classes] : nil,
         append_classes: append_classes ? [append_classes] : nil,
+        id:,
         size:,
         responsive_extra_small_size:,
         responsive_small_size:,
@@ -66,19 +70,13 @@ module AdminSite
     # @!group Behaviors
 
     # @label Active
-    def behavior_active(content: 'Active', url: '#')
-      render COMPONENT_CLASS.new(
-        active: true,
-        url:
-      ).with_content(content)
+    def behavior_active
+      default(active: true, content: 'Active', url: '#')
     end
 
     # @label Disabled
-    def behavior_disabled(content: 'Disabled', url: '#')
-      render COMPONENT_CLASS.new(
-        disabled: true,
-        url:
-      ).with_content(content)
+    def behavior_disabled
+      default(disabled: true, content: 'Disabled', url: '#')
     end
 
     # @!endgroup
@@ -86,43 +84,28 @@ module AdminSite
     # @!group Sizes
 
     # @label Extra Small
-    def size_extra_small(content: 'Extra Small', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_EXTRA_SMALL,
-        url:
-      ).with_content(content)
+    def size_extra_small
+      default(size: COMPONENT_CLASS::SIZE_EXTRA_SMALL, content: 'Extra Small', url: '#')
     end
 
     # @label Small
-    def size_small(content: 'Small', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_SMALL,
-        url:
-      ).with_content(content)
+    def size_small
+      default(size: COMPONENT_CLASS::SIZE_SMALL, content: 'Small', url: '#')
     end
 
     # @label Medium
-    def size_medium(content: 'Medium', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_MEDIUM,
-        url:
-      ).with_content(content)
+    def size_medium
+      default(size: COMPONENT_CLASS::SIZE_MEDIUM, content: 'Medium', url: '#')
     end
 
     # @label Large
-    def size_large(content: 'Large', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_LARGE,
-        url:
-      ).with_content(content)
+    def size_large
+      default(size: COMPONENT_CLASS::SIZE_LARGE, content: 'Large', url: '#')
     end
 
     # @label Extra Large
-    def size_extra_large(content: 'Extra Large', url: '#')
-      render COMPONENT_CLASS.new(
-        size: COMPONENT_CLASS::SIZE_EXTRA_LARGE,
-        url:
-      ).with_content(content)
+    def size_extra_large
+      default(size: COMPONENT_CLASS::SIZE_EXTRA_LARGE, content: 'Extra Large', url: '#')
     end
 
     # @!endgroup
@@ -130,43 +113,23 @@ module AdminSite
     # @!group Modifiers
 
     # @label Wide
-    def modifier_wide(content: 'Wide', url: '#')
-      render COMPONENT_CLASS.new(
-        wide: true,
-        url:
-      ).with_content(content)
+    def modifier_wide
+      default(wide: true, content: 'Wide', url: '#')
     end
 
     # @label Block
-    def modifier_block(content: 'Block', url: '#')
-      render COMPONENT_CLASS.new(
-        block: true,
-        url:
-      ).with_content(content)
+    def modifier_block
+      default(block: true, content: 'Block', url: '#')
     end
 
     # @label Square
-    def modifier_square(url: '#')
-      render COMPONENT_CLASS.new(
-        square: true,
-        url:
-      ).with_content(
-        render_component_outside_view_context(
-          FontAwesome::CircleInfoIconComponent.new
-        )
-      )
+    def modifier_square
+      default(square: true, content: SVG_ICON.html_safe, url: '#') # rubocop:disable Rails/OutputSafety
     end
 
     # @label Circle
-    def modifier_circle(url: '#')
-      render COMPONENT_CLASS.new(
-        circle: true,
-        url:
-      ).with_content(
-        render_component_outside_view_context(
-          FontAwesome::CircleInfoIconComponent.new
-        )
-      )
+    def modifier_circle
+      default(circle: true, content: SVG_ICON.html_safe, url: '#') # rubocop:disable Rails/OutputSafety
     end
 
     # @!endgroup
