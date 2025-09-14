9f3bbeaeded1c5201b522fb71c0c09fd419ad33c
greendrop <greendrop.devel@gmail.com>
2025-09-14 04:11:53 +0000

    feat: AdminSiteにFieldsetComponent、FieldsetLegendComponent、FloatingLabelComponentを追加し、スナップショットテストを実装

---
+++ spec/components/admin_site/fieldset_component_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AdminSite::FieldsetComponent, type: :component do
+  describe 'snapshot' do
+    include ActionView::Helpers::OutputSafetyHelper
+
+    let!(:snapshot_prefix) { described_class.name.underscore }
+    let!(:rendered) { render_inline(instance) }
+
+    shared_examples 'match snapshot' do
+      it { expect(rendered).to match_snapshot(snapshot_name) }
+    end
+
+    context 'default' do
+      let!(:instance) do
+        described_class.new.with_content(
+          safe_join(
+            [
+              render_component_outside_view_context(
+                AdminSite::FieldsetLegendComponent.new.with_content('legend')
+              ),
+              render_component_outside_view_context(
+                AdminSite::LabelComponent.new.with_content('label')
+              ),
+              render_component_outside_view_context(
+                AdminSite::TextFieldComponent.new(name: 'input_name')
+              ),
+              render_component_outside_view_context(
+                AdminSite::LabelComponent.new(tag_name: 'p').with_content('hint')
+              )
+            ]
+          )
+        )
+      end
+      let!(:snapshot_name) { File.join(snapshot_prefix, 'default') }
+
+      it_behaves_like 'match snapshot'
+    end
+  end
+end
