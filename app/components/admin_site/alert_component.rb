af1232f88984376d6826eea5a6a5b4478855f5f0
greendrop <greendrop.devel@gmail.com>
2025-09-24 05:10:04 +0000

    feat: AdminSiteにアラートコンポーネントとそのスナップショットテストを追加

---
+++ app/components/admin_site/alert_component.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module AdminSite
+  class AlertComponent < DaisyUi::AlertComponent
+  end
+end
---
+++ app/components/admin_site/alert_text_component.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module AdminSite
+  class AlertTextComponent < DaisyUi::AlertTextComponent
+  end
+end
